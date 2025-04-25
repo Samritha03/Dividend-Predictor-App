ef3b022c65019a6590a68bef7b7eab7e214d9d68
Samritha <103274678+Samritha03@users.noreply.github.com>
2025-04-25 17:11:17 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,39 @@
+# # File: home.py
+# import streamlit as st
+
+# # Set page configuration
+# st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
+
+# # --- Background image CSS ---
+# page_bg_img = '''
+# <style>
+# .stApp {
+#     background-image: url('https://www.pristinemarketinsights.com/assets/images/banking-and-finance-bg.jpg');
+#     background-size: cover;
+#     background-position: center;
+#     background-repeat: no-repeat;
+#     background-attachment: fixed;
+# }
+# </style>
+# '''
+
+# st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# # --- Render content ---
+# st.title("📊 Dividend Trend Predictor")
+
+# st.markdown("""
+# <span style='font-size: 24px; font-weight: bold;'>BA870 Financial Analytics - Final Project</span>
+
+# <br>
+
+# Team Members:
+# - Samritha Aadhi Ravikumar MSDS'25
+# - Akshara Ramprasad MSDS'25
+# - Crystal Leatvanich MSBA'25
+# """, unsafe_allow_html=True)
+
+
 # File: home.py
 import streamlit as st
 
@@ -16,7 +52,6 @@ page_bg_img = '''
 }
 </style>
 '''
-
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # --- Render content ---
@@ -25,10 +60,12 @@ st.title("📊 Dividend Trend Predictor")
 st.markdown("""
 <span style='font-size: 24px; font-weight: bold;'>BA870 Financial Analytics - Final Project</span>
 
-<br>
+<br><br>
 
-Team Members:
-- Samritha Aadhi Ravikumar MSDS'25
-- Akshara Ramprasad MSDS'25
-- Crystal Leatvanich MSBA'25
+<span style='font-size: 18px; font-weight: bold;'>Team Members:</span><br>
+<ul style='font-size: 18px; font-weight: bold;'>
+  <li>Samritha Aadhi Ravikumar MSDS'25</li>
+  <li>Akshara Ramprasad MSDS'25</li>
+  <li>Crystal Leatvanich MSBA'25</li>
+</ul>
 """, unsafe_allow_html=True)
