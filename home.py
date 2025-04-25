f696ee8b7a54888c428d4ec0377466c964cff8c9
Samritha <103274678+Samritha03@users.noreply.github.com>
2025-04-25 08:58:30 -0400

    Update home.py

--- home.py
+++ home.py
@@ -16,7 +16,8 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url('https://images.unsplash.com/photo-1508780709619-79562169bc64');
+        # background-image: url('https://images.unsplash.com/photo-1508780709619-79562169bc64');
+        background-image: url('https://www.google.com/search?q=divident+prediction+&sca_esv=19f8f2d2dec41c53&rlz=1C5CHFA_enIN994IN994&udm=2&biw=1384&bih=693&sxsrf=AHTn8zrHWRUL2mczlsT6Gk22IR_MXWBpyg%3A1745584328437&ei=yIALaPm7Gsm9ptQPqbzLkQs&ved=0ahUKEwj5v8mumPOMAxXJnokEHSneMrIQ4dUDCBE&oq=divident+prediction+&gs_lp=EgNpbWciFGRpdmlkZW50IHByZWRpY3Rpb24gSI5vUL8PWPlLcAJ4AJABAJgBTaABrAiqAQIxNrgBDMgBAPgBAZgCDaACvQaoAgrCAgoQABiABBhDGIoFwgIGEAAYBxgewgIKECMYJxjJAhjqAsICDhAAGIAEGLEDGIMBGIoFwgILEAAYgAQYsQMYgwHCAgUQABiABMICCBAAGIAEGLEDwgINEAAYgAQYsQMYQxiKBcICChAAGIAEGLEDGArCAg0QABiABBixAxiDARgKwgIHEAAYgAQYCpgDBYgGAZIHAjEzoAebR7IHAjExuAeyBg&sclient=img#imgrc=U0xQQxIsWmRKRM&imgdii=BznL36nPCiNuCM');
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
@@ -48,8 +49,8 @@ st.markdown("""
 <br>
 
 Team Members:
+- Akshara Ramprasad MSDS'25Samritha Aadhi Ravikumar MSDS'25
 - Akshara Ramprasad MSDS'25
-- Samritha Aadhi Ravikumar MSDS'25
 - Crystal Leatvanich MSBA'25
 """, unsafe_allow_html=True)
 
