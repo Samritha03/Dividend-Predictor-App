5970d242080a7988f4eeffcf33be75589318dc81
Samritha <103274678+Samritha03@users.noreply.github.com>
2025-04-25 16:51:14 -0400

    Update home.py

--- home.py
+++ home.py
@@ -4,10 +4,9 @@ import streamlit as st
 # Set page configuration
 st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
-# --- Custom CSS ---
-st.markdown("""
+# --- Background image CSS ---
+page_bg_img = '''
 <style>
-/* App-wide background image */
 .stApp {
     background-image: url('https://images.unsplash.com/photo-1508780709619-79562169bc64');
     background-size: cover;
@@ -15,29 +14,20 @@ st.markdown("""
     background-repeat: no-repeat;
     background-attachment: fixed;
 }
-
-/* Make all text inside white */
-h1, h2, h3, p, li, span {
-    color: white !important;
-}
 </style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# --- Render content ---
+st.title("📊 Dividend Trend Predictor")
+
+st.markdown("""
+<span style='font-size: 24px; font-weight: bold;'>BA870 Financial Analytics - Final Project</span>
+
+<br>
+
+Team Members:
+- Samritha Aadhi Ravikumar MSDS'25
+- Akshara Ramprasad MSDS'25
+- Crystal Leatvanich MSBA'25
 """, unsafe_allow_html=True)
-
-# --- Main Content ---
-with st.container():
-    st.markdown("<div class='main-block'>", unsafe_allow_html=True)
-
-    st.title("📊 Dividend Trend Predictor")
-
-    st.markdown("""
-    <span style='font-size: 24px; font-weight: bold;'>BA870 Financial Analytics - Final Project</span>
-
-    <br><br>
-
-    Team Members:
-    - Samritha Aadhi Ravikumar MSDS'25
-    - Akshara Ramprasad MSDS'25
-    - Crystal Leatvanich MSBA'25
-    """, unsafe_allow_html=True)
-
-    st.markdown("</div>", unsafe_allow_html=True)
