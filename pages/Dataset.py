c384794451d8f7d0179a01800077d893a495b31a
Samritha <103274678+Samritha03@users.noreply.github.com>
2025-04-25 13:56:36 -0400

    Update Dataset.py

--- pages/Dataset.py
+++ pages/Dataset.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 st.title("🗂️ Our Dataset")
 
 st.markdown(" Access our Colab notebook here : https://colab.research.google.com/drive/1Ocjx5k9_szTnyFYZKBLshATQ1fFTpbeV?usp=sharing")
+st.markdown(" Access our github repo here : https://github.com/Samritha03/Dividend-Predictor-App/tree/main")
 # --- Load training dataset ---
 try:
     df = pd.read_csv("data/dividends.csv")
